1e01960c18833d3748121424a8449086da813641
Sparrow007 <ankit.steven007@gmail.com>
2021-03-31 15:31:45 +0530

    adding the news repository interface

--- app/src/main/java/com/jackandphantom/libtest/MVP/repository/entity/NewsRepository.kt
+++ app/src/main/java/com/jackandphantom/libtest/MVP/repository/entity/NewsRepository.kt
@@ -2,7 +2,7 @@ package com.jackandphantom.libtest.MVP.repository.entity
 
 interface NewsRepository {
 
-    fun byId(id: Long): News
+    fun byId(id: Long): News?
 
     fun getAllList(): List<News>
 
