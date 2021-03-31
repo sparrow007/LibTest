e433119176cb525ed83c7b610059bd1c3a18b57f
Sparrow007 <ankit.steven007@gmail.com>
2021-03-31 15:08:31 +0530

    adding interface setup for mvp:

---
+++ app/src/main/java/com/jackandphantom/libtest/MVP/InitApp.kt
@@ -0,0 +1,4 @@
+package com.jackandphantom.libtest.MVP
+
+class InitApp {
+}
---
+++ app/src/main/java/com/jackandphantom/libtest/MVP/repository/entity/News.kt
@@ -0,0 +1,7 @@
+package com.jackandphantom.libtest.MVP.repository.entity
+
+data class News (
+    val id : Long,
+    val title: String,
+    val body: String
+)
