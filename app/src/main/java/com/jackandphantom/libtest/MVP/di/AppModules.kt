5980de0f29b8fb45a9949156dffd90f4a836ac4c
Sparrow007 <ankit.steven007@gmail.com>
2021-04-01 11:15:01 +0530

    adding the app modules

--- app/src/main/java/com/jackandphantom/libtest/MVP/di/AppModules.kt
+++ app/src/main/java/com/jackandphantom/libtest/MVP/di/AppModules.kt
@@ -1,6 +1,23 @@
 package com.jackandphantom.libtest.MVP.di
 
+import com.jackandphantom.libtest.MVP.presenter.impl.NewsDetailsImpl
+import com.jackandphantom.libtest.MVP.presenter.impl.NewsListImpl
+import com.jackandphantom.libtest.MVP.repository.NewsRepository
+import com.jackandphantom.libtest.MVP.repository.repoimpl.MemoryRepository
+import dagger.Module
+import dagger.Provides
 
 
+@Module
 class AppModules {
+
+    @Provides
+    fun getNewsDetails(newsRepository: NewsRepository) = NewsDetailsImpl(newsRepository)
+
+    @Provides
+    fun getNewsList(newsRepository: NewsRepository) = NewsListImpl(newsRepository)
+
+    @Provides
+    fun getNewsRepository() : NewsRepository = MemoryRepository()
+
 }
