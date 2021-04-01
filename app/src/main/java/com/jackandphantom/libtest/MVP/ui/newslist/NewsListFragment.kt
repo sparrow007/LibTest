c4a8172902dc98a3c5dce31ef6d3df181786430a
Sparrow007 <ankit.steven007@gmail.com>
2021-04-01 11:19:28 +0530

    adding newslist and updting the newsDetailsFragment

--- app/src/main/java/com/jackandphantom/libtest/MVP/ui/newslist/NewsListFragment.kt
+++ app/src/main/java/com/jackandphantom/libtest/MVP/ui/newslist/NewsListFragment.kt
@@ -1,4 +1,4 @@
-package com.jackandphantom.libtest.MVP.ui.newsdetails
+package com.jackandphantom.libtest.MVP.ui.newslist
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,15 +7,11 @@ import android.view.View
 import android.view.ViewGroup
 import com.jackandphantom.libtest.R
 
-/**
- * A simple [Fragment] subclass.
- * Use the [NewsDetailsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class NewsDetailsFragment : Fragment() {
+class NewsListFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
@@ -23,7 +19,7 @@ class NewsDetailsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news_details, container, false)
+        return inflater.inflate(R.layout.fragment_news_list, container, false)
     }
 
 }
