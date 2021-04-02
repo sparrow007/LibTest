6382f951a79b731fb8818684596ca2d49fec297d
Sparrow007 <ankit.steven007@gmail.com>
2021-04-02 09:58:40 +0530

    update the mvp

--- app/src/main/java/com/jackandphantom/libtest/MVP/ui/newslist/RecyclerViewHolder.kt
+++ app/src/main/java/com/jackandphantom/libtest/MVP/ui/newslist/RecyclerViewHolder.kt
@@ -2,8 +2,12 @@ package com.jackandphantom.libtest.MVP.ui.newslist
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.jackandphantom.common.event.OnViewHolderItemSelect
+import com.jackandphantom.libtest.MVP.repository.entity.News
 
-class RecyclerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class RecyclerViewHolder(
+        private val itemView: View,
+        listener: OnViewHolderItemSelect<News>? = null) : RecyclerView.ViewHolder(itemView) {
 
     fun bind(model: News) {
 
