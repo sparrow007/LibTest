3fc7fad66d0cb1e2ec83de0e06ccd1222d154077
Sparrow007 <ankit.steven007@gmail.com>
2021-04-02 10:55:28 +0530

    updating the details fragment

--- app/src/main/java/com/jackandphantom/libtest/MVP/ui/newsdetails/NewsDetailsFragment.kt
+++ app/src/main/java/com/jackandphantom/libtest/MVP/ui/newsdetails/NewsDetailsFragment.kt
@@ -1,22 +1,29 @@
 package com.jackandphantom.libtest.MVP.ui.newsdetails
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.jackandphantom.libtest.MVP.di.DaggerAppComponent
+import com.jackandphantom.libtest.MVP.presenter.NewsDetailsPresenter
+import com.jackandphantom.libtest.MVP.repository.entity.News
 import com.jackandphantom.libtest.R
+import javax.inject.Inject
 
 /**
  * A simple [Fragment] subclass.
  * Use the [NewsDetailsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class NewsDetailsFragment : Fragment() {
+class NewsDetailsFragment : Fragment(), NewsDetailsView {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+   @Inject
+   lateinit var newsDetailsPresenter: NewsDetailsPresenter
+    private lateinit var newsTitleTextView: TextView
+    private lateinit var newsBodyTextView: TextView
 
     companion object {
         const val NEWS_ID = "news_id"
@@ -26,12 +33,29 @@ class NewsDetailsFragment : Fragment() {
         }
     }
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        DaggerAppComponent.create().inject(this)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_news_details, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        newsDetailsPresenter.bind(this)
+
+        newsTitleTextView = view.findViewById(R.id.news_detail_title)
+        newsBodyTextView = view.findViewById(R.id.news_detail_body)
+    }
+
+    override fun displayNews(news: News) {
+
     }
 
 }
