8621b89319bf45a424e761c0e7ebf49745f4799a
Sparrow007 <ankit.steven007@gmail.com>
2021-04-02 11:52:11 +0530

    adding fragment to the host

--- app/src/main/java/com/jackandphantom/libtest/MVP/MVPActivity.kt
+++ app/src/main/java/com/jackandphantom/libtest/MVP/MVPActivity.kt
@@ -2,11 +2,13 @@ package com.jackandphantom.libtest.MVP
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jackandphantom.libtest.MVP.ui.newslist.NewsListFragment
 import com.jackandphantom.libtest.R
 
 class MVPActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_m_v_p)
+        supportFragmentManager.beginTransaction().replace(R.id.anchor, NewsListFragment()).commit()
     }
 }
