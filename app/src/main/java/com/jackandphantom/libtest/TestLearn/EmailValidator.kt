8f920ea60b403e7bc639503fb040725d62864922
Sparrow007 <ankit.steven007@gmail.com>
2021-04-15 06:53:04 +0530

    adding test for email validate

---
+++ app/src/main/java/com/jackandphantom/libtest/TestLearn/EmailValidator.kt
@@ -0,0 +1,4 @@
+package com.jackandphantom.libtest.TestLearn
+
+object EmailValidator {
+}
---
+++ app/src/test/java/com/jackandphantom/libtest/EmailValidatorTest.kt
@@ -0,0 +1,15 @@
+package com.jackandphantom.libtest
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+
+
+class EmailValidatorTest {
+
+    @Test
+    fun emailValidator_CorrectEmailSimple_ReturnTrue() {
+      // assertThat(EmailValidate)
+    }
+
+}
