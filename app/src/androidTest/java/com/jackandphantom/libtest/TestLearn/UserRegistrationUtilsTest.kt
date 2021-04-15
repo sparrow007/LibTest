7bff2fec4ff6067f44f7279e69e9b6effb2a0a47
Sparrow007 <ankit.steven007@gmail.com>
2021-04-16 00:16:23 +0530

    adding resource initialize in test

--- app/src/androidTest/java/com/jackandphantom/libtest/TestLearn/UserRegistrationUtilsTest.kt
+++ app/src/androidTest/java/com/jackandphantom/libtest/TestLearn/UserRegistrationUtilsTest.kt
@@ -6,17 +6,24 @@ import com.google.common.truth.Truth
 import com.jackandphantom.libtest.R
 import org.junit.Test
 
+/**
+ * Unit Instrument Test
+ */
+
 class UserRegistrationUtilsTest {
 
-    private val resourceComparare = UserRegistrationUtils()
+    //Each test cases should run independently
+    private lateinit var resourceComparare:UserRegistrationUtils
 
     @Test
     fun stringResourceSameAsProvidedString_ReturnTrue() {
+        resourceComparare = UserRegistrationUtils()
         val context = ApplicationProvider.getApplicationContext<Context>()
         Truth.assertThat(resourceComparare.validateUser(context, R.string.app_name, "LibTest")).isTrue()
     }
     @Test
     fun stringResourceNotSameAsProvidedString_ReturnFalse() {
+        resourceComparare = UserRegistrationUtils()
         val context = ApplicationProvider.getApplicationContext<Context>()
         Truth.assertThat(resourceComparare.validateUser(context, R.string.app_name, "LibTesting")).isFalse()
     }
