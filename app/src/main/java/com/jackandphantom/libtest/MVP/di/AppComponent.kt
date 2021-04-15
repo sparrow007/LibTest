d1a943058d39b91b185a790c2ccb563ac5a3bbe1
Sparrow007 <ankit.steven007@gmail.com>
2021-04-15 06:05:58 +0530

    refactor app component

--- app/src/main/java/com/jackandphantom/libtest/MVP/di/AppComponent.kt
+++ app/src/main/java/com/jackandphantom/libtest/MVP/di/AppComponent.kt
@@ -1,10 +1,5 @@
 package com.jackandphantom.libtest.MVP.di
 
-import com.jackandphantom.libtest.MVP.repository.NewsRepository
-import com.jackandphantom.libtest.MVP.repository.repoimpl.MemoryRepository
-import com.jackandphantom.libtest.MVP.ui.newsdetails.NewsDetailsFragment
-import com.jackandphantom.libtest.MVP.ui.newslist.NewsListFragment
-import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
 
