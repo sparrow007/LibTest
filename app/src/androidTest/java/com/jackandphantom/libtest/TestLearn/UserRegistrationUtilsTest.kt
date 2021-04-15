a71e9ff1e21d62ce66a63b668d7b474863da2b33
Sparrow007 <ankit.steven007@gmail.com>
2021-04-16 00:18:43 +0530

    adding back and after in testing

--- app/src/androidTest/java/com/jackandphantom/libtest/TestLearn/UserRegistrationUtilsTest.kt
+++ app/src/androidTest/java/com/jackandphantom/libtest/TestLearn/UserRegistrationUtilsTest.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import androidx.test.core.app.ApplicationProvider
 import com.google.common.truth.Truth
 import com.jackandphantom.libtest.R
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 
 /**
@@ -15,15 +17,26 @@ class UserRegistrationUtilsTest {
     //Each test cases should run independently
     private lateinit var resourceComparare:UserRegistrationUtils
 
+    /**
+     * Runs each time when particular test function are gets called
+     */
+    @Before
+    fun setup() {
+        resourceComparare = UserRegistrationUtils()
+    }
+
+    @After
+    fun tearDown() {
+        //Destroy the objects for example room database, you should have closed it.
+    }
+
     @Test
     fun stringResourceSameAsProvidedString_ReturnTrue() {
-        resourceComparare = UserRegistrationUtils()
         val context = ApplicationProvider.getApplicationContext<Context>()
         Truth.assertThat(resourceComparare.validateUser(context, R.string.app_name, "LibTest")).isTrue()
     }
     @Test
     fun stringResourceNotSameAsProvidedString_ReturnFalse() {
-        resourceComparare = UserRegistrationUtils()
         val context = ApplicationProvider.getApplicationContext<Context>()
         Truth.assertThat(resourceComparare.validateUser(context, R.string.app_name, "LibTesting")).isFalse()
     }
