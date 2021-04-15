7c992502eff52461167a7cbdec06f253773d79e0
Sparrow007 <ankit.steven007@gmail.com>
2021-04-15 07:08:25 +0530

    passing email validator test

--- app/src/main/java/com/jackandphantom/libtest/TestLearn/EmailValidator.kt
+++ app/src/main/java/com/jackandphantom/libtest/TestLearn/EmailValidator.kt
@@ -1,4 +1,22 @@
 package com.jackandphantom.libtest.TestLearn
 
+import android.util.Patterns
+import java.util.regex.Pattern
+
 object EmailValidator {
+
+
+    /**
+     * Checks for email to not empty with validation of email
+     */
+    fun validate(email: String): Boolean {
+        return email.isNotEmpty() && Pattern.compile(
+            "^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]|[\\w-]{2,}))@"
+                    + "((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                    + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\."
+                    + "([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                    + "[0-9]{1,2}|25[0-5]|2[0-4][0-9]))|"
+                    + "([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$"
+        ).matcher(email).matches()
+    }
 }
--- app/src/test/java/com/jackandphantom/libtest/EmailValidatorTest.kt
+++ app/src/test/java/com/jackandphantom/libtest/EmailValidatorTest.kt
@@ -1,6 +1,7 @@
 package com.jackandphantom.libtest
 
 import com.google.common.truth.Truth.assertThat
+import com.jackandphantom.libtest.TestLearn.EmailValidator
 import org.junit.Test
 
 
@@ -9,7 +10,17 @@ class EmailValidatorTest {
 
     @Test
     fun emailValidator_CorrectEmailSimple_ReturnTrue() {
-      // assertThat(EmailValidate)
+       assertThat(EmailValidator.validate("ankit.loma@gmail.com")).isTrue()
+    }
+
+    @Test
+    fun emailValidator_IncorrectEmailSimple_ReturnFalse() {
+        assertThat(EmailValidator.validate("akakkala")).isFalse()
+    }
+
+    @Test
+    fun emailValidator_EmptyEmail_ReturnFalse() {
+        assertThat(EmailValidator.validate("")).isFalse()
     }
 
 }
