b62adcd9ce7af5c2b62bae14d5fb55a09e221e0d
Sparrow007 <ankit.steven007@gmail.com>
2021-04-17 08:06:36 +0530

    adding rxjava

---
+++ app/src/main/java/com/jackandphantom/libtest/RxJava/RxJavaActivity.kt
@@ -0,0 +1,11 @@
+package com.jackandphantom.libtest.RxJava
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RxJavaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rx_java)
+    }
+}
