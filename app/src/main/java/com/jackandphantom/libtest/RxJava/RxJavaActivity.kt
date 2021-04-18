c23df2a179696006acc16f149de45100a3bd177c
Sparrow007 <ankit.steven007@gmail.com>
2021-04-18 23:30:38 +0530

    adding postobservable

--- app/src/main/java/com/jackandphantom/libtest/RxJava/RxJavaActivity.kt
+++ app/src/main/java/com/jackandphantom/libtest/RxJava/RxJavaActivity.kt
@@ -2,23 +2,20 @@ package com.jackandphantom.libtest.RxJava
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.jackandphantom.libtest.R
-import com.jackandphantom.libtest.RxJava.Defer.DeferRxJava
-import com.jackandphantom.libtest.RxJava.model.Task
-import com.jackandphantom.libtest.RxJava.utils.DataSource
+import com.jackandphantom.libtest.RxJava.flatmap.RecyclerAdapter
+import com.jackandphantom.libtest.RxJava.flatmap.model.Post
+import com.jackandphantom.libtest.RxJava.flatmap.service.Service
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Observer
-import io.reactivex.rxjava3.core.Scheduler
-import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.schedulers.Schedulers
-import java.util.*
-import java.util.concurrent.TimeUnit
-import kotlin.math.log
 
 class RxJavaActivity : AppCompatActivity() {
 
@@ -26,8 +23,12 @@ class RxJavaActivity : AppCompatActivity() {
     lateinit var editText: EditText
     lateinit var button: Button
 
+    lateinit var recyclerview: RecyclerView
+
     lateinit var observable:Observable<String>
     lateinit var observer: Observer<String>
+    private val recyclerAdapter: RecyclerAdapter = RecyclerAdapter()
+    private val compositeDisposable = CompositeDisposable()
 
     companion object {
         const val TAG = "MY TAG RXJAVA"
@@ -35,12 +36,29 @@ class RxJavaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rx_java)
-
-
+        recyclerview = findViewById(R.id.recycler_view)
 
 
     }
 
+    private fun getPostObservable():Observable<Post> {
+        return Service.requestApi.getPost()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .flatMap {
+                recyclerAdapter.setPosts(it as MutableList<Post>)
+                return@flatMap Observable.fromIterable(it).subscribeOn(Schedulers.io())
+            }
+    }
 
+    private fun initRecyclerview() {
+        recyclerview.adapter = recyclerAdapter
+        recyclerview.layoutManager = LinearLayoutManager(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.clear()
+    }
 
 }
--- app/src/main/java/com/jackandphantom/libtest/RxJava/flatmap/service/RequestApi.kt
+++ app/src/main/java/com/jackandphantom/libtest/RxJava/flatmap/service/RequestApi.kt
@@ -2,14 +2,15 @@ package com.jackandphantom.libtest.RxJava.flatmap.service
 
 import com.jackandphantom.libtest.RxJava.flatmap.model.Comment
 import com.jackandphantom.libtest.RxJava.flatmap.model.Post
-import io.reactivex.Observable
+import io.reactivex.rxjava3.core.Observable
+
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface RequestApi {
 
     @GET("posts")
-    fun getPost(): Observable<Post>
+    fun getPost(): Observable<List<Post>>
 
     @GET("posts/{id}/comments")
     fun getComments(@Path("id")id: Int):Observable<Comment>
